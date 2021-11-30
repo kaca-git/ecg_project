e06a326316b6a298c3ee366e7ac9abb076d599e3
kaca-git <85035264+kaca-git@users.noreply.github.com>
2021-11-30 11:26:49 +0100

    Add files via upload

--- run_project.bat
+++ run_project.bat
@@ -1,5 +1,7 @@
 docker build -t docker_jl .
 docker run -dit --name jlc -p 8888:8888 docker_jl
 docker exec -it jlc wget https://zenodo.org/record/5711347/files/ECG_Rhythm_Lead_I.csv?download=1
-docker exec -it jlc mongoimport -d ecg_database -c ecg_signals --type csv --file ECG_Rhythm_Lead_I.csv --headerline
+docker exec -it jlc mongoimport -d ecg_database -c ecg_signals --type csv --file ECG_Rhythm_Lead_I.csv?download=1 --headerline
 docker exec -it jlc jupyter lab --port=8888 --no-browser --ip=0.0.0.0 --allow-root
+docker stop jlc
+docker rm jlc
